06606b9850a3592e5262657b5b507b737281dd30
Desi Mandasari <desimandasari1993@gmail.com>
2019-01-07 23:55:06 +0700

    create react-todos

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/header';
+import TodoInput from './components/todoInput';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        { id: 0, text: "Make dinner tonight!" },
+        { id: 1, text: "Clean makeup from my face" },
+        { id: 2, text: "Try to learn React!" }
+      ],
+      nextId: 3
+    }
+
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  addTodo(todoText) {
+    console.log("Todo added: ", todoText);
+  }
+
+  removeTodo(id) {
+    console.log("Removing: ", id);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="todo-wrapper">
+          <Header /> 
+          <TodoInput todoText="" addTodo={this.addTodo} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
